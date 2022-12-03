fa38e4fa3284f20145bc7d24f870f69f7f89068f
Sebastian Heil <sebastian.heil@1und1.de>
2022-12-03 19:02:30 +0100

    Improve Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,23 +1,25 @@
+import java.io.File
+
 fun main() {
-    fun caloriesByElves(input: List<String>): List<Int> {
-        return input
-            .joinToString("\n")
-            .split("\n\n")
-            .map { caloriesStrings ->
-                caloriesStrings.split("\n")
-                    .sumOf { string -> string.toInt() }
-            }
-    }
+    fun CharSequence.chunkedBy(predicate: () -> String): List<String> = split(predicate())
 
-    fun part1(input: List<String>): Int = caloriesByElves(input).max()
+    fun caloriesByElves(input: String): List<Int> = input
+        .chunkedBy { "\n\n" }
+        .map { caloriesStrings ->
+            caloriesStrings.lines().sumOf { it.toInt() }
+        }
 
-    fun part2(input: List<String>): Int = caloriesByElves(input).sorted().takeLast(3).sum()
+    fun sumHighestCalories(input: String, amount: Int): Int = caloriesByElves(input).sortedDescending().take(amount).sum()
 
-    val testInput = readInput("Day01_test")
+    fun part1(input: String): Int = sumHighestCalories(input,1)
+
+    fun part2(input: String): Int = sumHighestCalories(input, 3)
+
+    val testInput = File("src/Day01_test.txt").readText()
     println("part1(testInput): " + part1(testInput))
     check(part1(testInput) == 24000)
 
-    val input = readInput("Day01")
+    val input = File("src/Day01.txt").readText()
     println("part1(input): " + part1(input))
     println("part2(input): " + part2(input))
 }
